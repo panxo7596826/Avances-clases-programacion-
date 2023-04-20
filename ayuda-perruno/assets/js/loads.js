c24feabc2ed190c80a07b66e16923401eb91239a
toencina <107225390+toencina@users.noreply.github.com>
2023-04-19 20:13:19 -0400

    vali callampawn

--- ayuda-perruno/assets/js/loads.js
+++ ayuda-perruno/assets/js/loads.js
@@ -15,13 +15,32 @@ function loadData() {
             /*LECTURA DE LA DATA JSON*/
             console.log("Datos: ", data);
             const objJson = JSON.parse(data);
+            var cards = "";
             for (i = 0; i < objJson.length; i++) {
                 console.log("_________________");
                 console.log("Codigo: ", objJson[i].codigo);
                 console.log("Titulo: ", objJson[i].titulo);
                 console.log("Imagen: ", objJson[i].imagen);
                 console.log("Descripcion :", objJson[i].descripcion);
+                cards = `     ${cards}
+                <div class="col-sm">
+                <div id="card-${objJson[i].codigo}" class="card my-4" style="width: 18rem">
+                <img src="${objJson[i].imagen}" class="card-img-top" alt="..." />
+                <div class="card-body">
+                    <h5 class="card-title">${objJson[i].tituloCard} title</h5>
+                    <p class="card-text"> ${objJson[i].descripcion}
+                    </p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+                </div>
+            </div> `;
             }
+            
+            //OBTENCION DE LA ETIQUETA MEDIANTE JAVASCRIPT//
+            var cardsHTML = document.getElementById("cards");
+            //SOBREESCRIBIR TAG CARDS. CON INFORMACION GENERADA
+            //EN EL CICLO FOR, MEDIANTE LA LECTURA DE UN DATA .JSON//
+            cardsHTML.innerHTML = cards;
         })
         .catch((err) => {
             /*CAPTURA DE ERRORES*/
